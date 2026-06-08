8cb2033d4949c9eb852fdba83adb233cfbf89dd5
Aditya Singh Jadoun <adityasinghjadoun17@gmail.com>
2026-06-08 23:46:27 +0530

    applying linear regression model

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 import numpy as np
 
@@ -8,3 +10,10 @@ Y=df.iloc[:,-1].values
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
 
+
+
+from sklearn .linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,Y_train)
+
+Y_pred=regressor.predict(X_test)
