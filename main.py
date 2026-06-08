b9d93f6d575bce9686dd0c16ac934e0800aa42cf
Aditya Singh Jadoun <adityasinghjadoun17@gmail.com>
2026-06-08 23:45:03 +0530

    splitting database

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv('advertising.csv')
+X=df.iloc[:,:-1].values
+Y=df.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+
