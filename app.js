0f01d35aeba9a2e0fa6c07fa26f4a85f2191c008
Aditya Singh Jadoun <adityasinghjadoun17@gmail.com>
2026-06-11 18:07:37 +0530

    Back End JS

---
+++ app.js
@@ -0,0 +1,40 @@
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("input", updateProgress);
+});
+
+
+document.querySelectorAll('input[type="radio"]').forEach(radio => {
+    radio.addEventListener("change", updateProgress);
+});
+let progress=0
+const values=new Map();
+function updateProgress(event) {
+    const id = event.target.id;
+    const value = event.target.value;
+
+    if (value === "") {
+        values.delete(id);
+    } else {
+        values.set(id, value);
+    }
+
+    progress = (values.size / 13) * 100;
+
+    document.getElementById("progress-pct").innerText =
+        Math.round(progress) + "%";
+}
+function submitRecord(){
+
+}
+function clearForm() {
+    document.querySelectorAll('input[type="number"]').forEach(input => {
+        input.value = "";
+    });
+    document.querySelectorAll('input[type="radio"]').forEach(radio => {
+        radio.checked = false;
+    });
+    values.clear();
+    progress = 0;
+    document.getElementById("progress-pct").innerText = "0%";
+    console.log("Form Cleared");
+}
